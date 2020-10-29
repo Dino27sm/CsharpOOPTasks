842a9737e0a9e8af23729f1bf2bdc36c12ea8033
Mironov <mironov5712s@gmail.com>
2020-10-29 23:34:56 +0200

    Create StartUp.cs

---
+++ OOPInheritance29Oct2020Exercise/Person/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Person
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            Person personOne;
+            if(age <= 15)
+            {
+                personOne = new Child(name, age);
+            }
+            else
+            {
+                personOne = new Person(name, age);
+            }
+            Console.WriteLine(personOne);
+        }
+    }
+}
