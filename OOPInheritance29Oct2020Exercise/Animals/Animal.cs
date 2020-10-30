2bf21d10c5c894c5752b0608fa0a292983b39297
Mironov <mironov5712s@gmail.com>
2020-10-30 17:46:40 +0200

    Create Animal.cs

---
+++ OOPInheritance29Oct2020Exercise/Animals/Animal.cs
@@ -0,0 +1,24 @@
+﻿namespace Animals
+{
+    using System;
+    using System.Text;
+
+    public class Animal
+    {
+        private string animalName;
+        private int animalAge;
+        private string animaGender;
+
+        public Animal(string name, int age, string gender)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+        }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        
+        public virtual string ProduceSound() => string.Empty;
+    }
+}
