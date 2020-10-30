9f7b8cf8a65e96f41da8385276efdf118e8bcfcf
Mironov <mironov5712s@gmail.com>
2020-10-30 17:51:42 +0200

    Create Tomcat.cs

---
+++ OOPInheritance29Oct2020Exercise/Animals/Tomcat.cs
@@ -0,0 +1,14 @@
+﻿namespace Animals
+{
+    class Tomcat : Cat
+    {
+        private const string tomcatGender = "Male";
+
+        public Tomcat(string name, int age)
+            : base(name, age, tomcatGender)
+        {
+        }
+        public override string ProduceSound()
+            => "MEOW";
+    }
+}
