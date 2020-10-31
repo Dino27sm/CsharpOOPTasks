06dfad44d45d2c206927d0fafa492fe6b04f3176
Mironov <mironov5712s@gmail.com>
2020-10-31 22:00:31 +0200

    Create Box.cs

---
+++ OOPEncapsulation02Nov2020Exercise/T01.ClassBoxData/Box.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace T01.ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double boxL, double boxW, double boxH)
+        {
+            this.Length = boxL;
+            this.Width = boxW;
+            this.Height = boxH;
+        }
+
+        public double Length 
+        { 
+            get => length;
+            private set 
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                length = value;
+            }
+        }
+        public double Width 
+        { 
+            get => width;
+            private set 
+            { 
+                if(value <= 0)
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                width = value;
+            }
+        }
+        public double Height 
+        { 
+            get => height;
+            private set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                height = value;
+            }
+        }
+
+        public double BoxLateralSurface() => (2 * (length + width) * height);
+
+        public double BoxSurface() => (BoxLateralSurface() + 2 * length * width);
+
+        public double BoxVolume() => (length * width * height);
+    }
+}
