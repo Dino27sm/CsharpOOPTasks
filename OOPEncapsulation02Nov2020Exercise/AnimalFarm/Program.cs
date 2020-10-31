a32adbf51c030ec3c55373576915a470bfe62181
Mironov <mironov5712s@gmail.com>
2020-10-31 22:05:04 +0200

    Create Program.cs

---
+++ OOPEncapsulation02Nov2020Exercise/AnimalFarm/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace AnimalFarm
+{
+    using System;
+    using AnimalFarm.Models;
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            Chicken chicken = null;
+            try
+            {
+                chicken = new Chicken(name, age);
+            }
+            catch (ArgumentException argException)
+            {
+                Console.WriteLine(argException.Message);
+                return;
+            }
+            Console.WriteLine(
+                "Chicken {0} (age {1}) can produce {2} eggs per day.",
+                chicken.Name,
+                chicken.Age,
+                chicken.ProductPerDay);
+        }
+    }
+}
