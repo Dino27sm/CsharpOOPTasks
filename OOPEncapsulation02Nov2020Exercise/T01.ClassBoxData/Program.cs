3df4ff88b2ef9410ebe5a4f61603bafb2890fc93
Mironov <mironov5712s@gmail.com>
2020-10-31 22:01:20 +0200

    Create Program.cs

---
+++ OOPEncapsulation02Nov2020Exercise/T01.ClassBoxData/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace T01.ClassBoxData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double boxLength = double.Parse(Console.ReadLine());
+            double boxWidth = double.Parse(Console.ReadLine());
+            double boxHeight = double.Parse(Console.ReadLine());
+
+            Box boxObject = null;
+            try
+            {
+                boxObject = new Box(boxLength, boxWidth, boxHeight);
+
+            }
+            catch (ArgumentException ae)
+            {
+
+                Console.WriteLine(ae.Message);
+                return;
+            }
+
+            Console.WriteLine($"Surface Area - {boxObject.BoxSurface():F2}");
+            Console.WriteLine($"Lateral Surface Area - {boxObject.BoxLateralSurface():F2}");
+            Console.WriteLine($"Volume - {boxObject.BoxVolume():F2}");
+        }
+    }
+}
