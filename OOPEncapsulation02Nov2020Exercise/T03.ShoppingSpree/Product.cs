dee2e1eaa268bd81a7a9d40a08f970f06592bfa3
Mironov <mironov5712s@gmail.com>
2020-11-01 18:46:50 +0200

    Create Product.cs

---
+++ OOPEncapsulation02Nov2020Exercise/T03.ShoppingSpree/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace T03.ShoppingSpree
+{
+    public class Product
+    {
+        private string name;
+        private double cost;
+
+        internal Product(string name, double cost)
+        {
+            this.Name = name;
+            this.Cost = cost;
+        }
+
+        internal string Name 
+        {
+            get => this.name;
+            set => this.name = !string.IsNullOrWhiteSpace(value) ? value : throw new ArgumentException("Name cannot be empty");
+        }
+        internal double Cost 
+        {
+            get => this.cost;
+            set => this.cost = value >= 0 ? value : throw new ArgumentException("Money cannot be negative");
+        }
+    }
+}
