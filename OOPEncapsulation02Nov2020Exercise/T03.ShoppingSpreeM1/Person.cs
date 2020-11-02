9256f3beeeb884edfddf5de766cea0f21e84fd3a
Mironov <mironov5712s@gmail.com>
2020-11-02 21:04:13 +0200

    Create Person.cs

---
+++ OOPEncapsulation02Nov2020Exercise/T03.ShoppingSpreeM1/Person.cs
@@ -0,0 +1,46 @@
+﻿namespace T03.ShoppingSpreeM1
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Person
+    {
+        private string name;
+        private decimal money;
+        private List<string> bag;
+
+        internal Person(string namePerson, decimal money)
+        {
+            this.Name = namePerson;
+            this.Money = money;
+            this.bag = new List<string>();
+        }
+        internal int BagCount => this.bag.Count;
+        internal string Name
+        {
+            get => this.name;
+            set => this.name = !string.IsNullOrWhiteSpace(value) ? value : throw new ArgumentException("Name cannot be empty");
+        }
+        internal decimal Money
+        {
+            get => this.money;
+            set => this.money = value >= 0M ? value : throw new ArgumentException("Money cannot be negative");
+        }
+
+        internal bool CanAfordProduct(Product productObject)
+        {
+            return (this.money - productObject.Cost >= 0M);
+        }
+
+        internal void BuyProduct(Product productObject)
+        {
+            this.bag.Add(productObject.Name);
+            this.Money -= productObject.Cost;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Name} - {string.Join(", ", this.bag)}";
+        }
+    }
+}
