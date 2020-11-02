377056a095eb68f7c663a1534bca4e9a363532c5
Mironov <mironov5712s@gmail.com>
2020-11-02 21:05:08 +0200

    Create Product.cs

---
+++ OOPEncapsulation02Nov2020Exercise/T03.ShoppingSpreeM1/Product.cs
@@ -0,0 +1,27 @@
+﻿namespace T03.ShoppingSpreeM1
+{
+    using System;
+
+    public class Product
+    {
+        private string name;
+        private decimal cost;
+
+        internal Product(string name, decimal cost)
+        {
+            this.Name = name;
+            this.Cost = cost;
+        }
+
+        internal string Name
+        {
+            get => this.name;
+            set => this.name = !string.IsNullOrWhiteSpace(value) ? value : throw new ArgumentException("Name cannot be empty");
+        }
+        internal decimal Cost
+        {
+            get => this.cost;
+            set => this.cost = value >= 0M ? value : throw new ArgumentException("Money cannot be negative");
+        }
+    }
+}
