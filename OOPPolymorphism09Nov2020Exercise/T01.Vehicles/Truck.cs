01c6512de3b03e1d6a085413d82c76aff069bdc7
Mironov <mironov5712s@gmail.com>
2020-11-08 18:10:56 +0200

    Vehicle file created

---
+++ OOPPolymorphism09Nov2020Exercise/T01.Vehicles/Truck.cs
@@ -0,0 +1,18 @@
+﻿namespace T01.Vehicles
+{
+    internal class Truck : Vehicle
+    {
+        private const double REFUEL_MODIFIER = 0.95;
+        private const double CUNSUMPTION_MODIFIER = 1.60;
+
+        internal Truck(double fuel, double consumption)
+            : base(fuel, consumption + CUNSUMPTION_MODIFIER)
+        {
+        }
+
+        internal override void Refuel(double fuelLoad)
+        {
+            base.Refuel(fuelLoad * REFUEL_MODIFIER);
+        }
+    }
+}
---
+++ OOPPolymorphism09Nov2020Exercise/T01.Vehicles/Vehicle.cs
@@ -0,0 +1,35 @@
+﻿namespace T01.Vehicles
+{
+    using System;
+
+    internal class Vehicle
+    {
+        internal double Fuel { get; set; }
+        internal double FuelConsumptionKm { get; set; }
+
+        internal Vehicle(double fuel, double consumption)
+        {
+            this.Fuel = fuel;
+            this.FuelConsumptionKm = consumption;
+        }
+
+        internal void Drive(double distance)
+        {
+            double consumedFuel = this.FuelConsumptionKm * distance;
+            if (consumedFuel <= this.Fuel)
+            {
+                this.Fuel -= consumedFuel;
+                Console.WriteLine($"{this.GetType().Name} travelled {distance} km");
+            }
+            else
+            {
+                Console.WriteLine($"{this.GetType().Name} needs refueling");
+            }
+        }
+
+        internal virtual void Refuel(double fuelLoad)
+        {
+            this.Fuel += fuelLoad;
+        }
+    }
+}
