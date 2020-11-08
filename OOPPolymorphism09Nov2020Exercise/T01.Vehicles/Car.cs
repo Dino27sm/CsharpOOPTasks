5638edd9b4844f90acc1419b0a3f8558aed0a89d
Mironov <mironov5712s@gmail.com>
2020-11-08 18:13:36 +0200

    Create Car.cs

---
+++ OOPPolymorphism09Nov2020Exercise/T01.Vehicles/Car.cs
@@ -0,0 +1,17 @@
+﻿namespace T01.Vehicles
+{
+    internal class Car : Vehicle
+    {
+        private const double CUNSUMPTION_MODIFIER = 0.9;
+
+        internal Car(double fuel, double consumption)
+            : base(fuel, consumption + CUNSUMPTION_MODIFIER)
+        {
+        }
+
+        internal override void Refuel(double fuelLoad)
+        {
+            base.Refuel(fuelLoad);
+        }
+    }
+}
