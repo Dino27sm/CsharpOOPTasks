ad5c6c276264122b9833e30c2a79b00d9013514e
Mironov <mironov5712s@gmail.com>
2020-11-09 13:47:58 +0200

    Create Truck.cs

---
+++ OOPPolymorphism09Nov2020Exercise/T01.VehiclesM1/Truck.cs
@@ -0,0 +1,24 @@
+﻿namespace T01.VehiclesM1
+{
+    internal class Truck : Vehicle
+    {
+        private const double CONSUMPTION_MODIFIER = 1.6;
+        private const double REFUEL_MODIFIER = 0.95;
+
+        public Truck(double fuel, double consumption) 
+            : base(fuel, consumption)
+        {
+        }
+
+        internal override double FuelConsumption 
+        { 
+            get => base.FuelConsumption; 
+            set => base.FuelConsumption = value + CONSUMPTION_MODIFIER; 
+        }
+
+        internal override void Refuel(double fuelLoad)
+        {
+            base.Refuel(fuelLoad * REFUEL_MODIFIER);
+        }
+    }
+}
