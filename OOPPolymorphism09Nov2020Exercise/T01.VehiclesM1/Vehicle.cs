8c7c2ab74c36b787cf374bc9dc5e2d7e3f342e15
Mironov <mironov5712s@gmail.com>
2020-11-09 13:45:07 +0200

    Create Vehicle.cs

---
+++ OOPPolymorphism09Nov2020Exercise/T01.VehiclesM1/Vehicle.cs
@@ -0,0 +1,37 @@
+﻿namespace T01.VehiclesM1
+{
+    internal abstract class Vehicle
+    {
+        public Vehicle(double fuel, double consumption)
+        {
+            this.FuelAmount = fuel;
+            this.FuelConsumption = consumption;
+        }
+
+        internal double FuelAmount { get; set; }
+        internal virtual double FuelConsumption { get; set; }
+
+        internal void Drive(double distance)
+        {
+            double fuelSpent = this.FuelConsumption * distance;
+            if (fuelSpent <= this.FuelAmount)
+            {
+                this.FuelAmount -= fuelSpent;
+                System.Console.WriteLine($"{this.GetType().Name} travelled {distance} km");
+            }
+            else
+            {
+                System.Console.WriteLine($"{this.GetType().Name} needs refueling");
+            }
+        }
+        internal virtual void Refuel(double fuelLoad)
+        {
+            this.FuelAmount += fuelLoad;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.GetType().Name}: {this.FuelAmount:F2}";
+        }
+    }
+}
