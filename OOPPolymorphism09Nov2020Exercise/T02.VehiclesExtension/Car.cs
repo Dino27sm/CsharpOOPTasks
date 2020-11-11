78126a938f8180edbe09981f0e90ceacca3a0f30
Mironov <mironov5712s@gmail.com>
2020-11-11 14:27:47 +0200

    Create Car.cs

---
+++ OOPPolymorphism09Nov2020Exercise/T02.VehiclesExtension/Car.cs
@@ -0,0 +1,12 @@
+﻿namespace T02.VehiclesExtension
+{
+    internal class Car : Vehicle
+    {
+        private const double CONSUMPTION_MODIFIER = 0.9;
+
+        public Car(double fuel, double consumption, double capacity)
+            : base(fuel, consumption + CONSUMPTION_MODIFIER, capacity)
+        {
+        }
+    }
+}
