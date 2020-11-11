01eeb22fbac2c6be88cd7aeed51f170e3b331005
Mironov <mironov5712s@gmail.com>
2020-11-11 14:26:19 +0200

    Create Bus.cs

---
+++ OOPPolymorphism09Nov2020Exercise/T02.VehiclesExtension/Bus.cs
@@ -0,0 +1,19 @@
+﻿namespace T02.VehiclesExtension
+{
+    internal class Bus : Vehicle
+    {
+        private const double CONSUMPTION_MODIFIER = 1.4;
+
+        public Bus(double fuel, double consumption, double capacity)
+            : base(fuel, consumption + CONSUMPTION_MODIFIER, capacity)
+        {
+        }
+
+        public void DriveEmpty(double distance)
+        {
+            this.FuelConsumption -= CONSUMPTION_MODIFIER;
+            base.Drive(distance);
+            this.FuelConsumption += CONSUMPTION_MODIFIER;
+        }
+    }
+}
