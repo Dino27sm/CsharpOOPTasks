8d5f76e48b33ee1ea8ec4380b85922e58080827e
Mironov <mironov5712s@gmail.com>
2020-11-11 14:34:37 +0200

    Create BaseHero.cs

---
+++ OOPPolymorphism09Nov2020Exercise/T03.Raiding/BaseHero.cs
@@ -0,0 +1,19 @@
+﻿namespace T03.Raiding
+{
+    public abstract class BaseHero
+    {
+        private string name;
+        private int power;
+
+        public BaseHero(string name, int power)
+        {
+            this.Name = name;
+            this.Power = power;
+        }
+
+        public string Name { get; set; }
+        public int Power { get; set; }
+
+        public abstract string CastAbility();
+    }
+}
