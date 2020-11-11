21d24fa1930370ba5f7cc290658fbe9cd24a53b2
Mironov <mironov5712s@gmail.com>
2020-11-11 14:36:10 +0200

    Create FactoryHeroes.cs

---
+++ OOPPolymorphism09Nov2020Exercise/T03.Raiding/FactoryHeroes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace T03.Raiding
+{
+    public class FactoryHeroes
+    {
+        public BaseHero CreateHero(string heroName, string heroType)
+        {
+            BaseHero heroObject = null;
+            if (heroType == "Druid") 
+                heroObject = new Druid(heroName);
+            else if (heroType == "Paladin") 
+                heroObject = new Paladin(heroName);
+            else if (heroType == "Rogue") 
+                heroObject = new Rogue(heroName);
+            else if (heroType == "Warrior") 
+                heroObject = new Warrior(heroName);
+            else 
+                throw new ArgumentException("Invalid hero!");
+            return heroObject;
+        }
+    }
+}
