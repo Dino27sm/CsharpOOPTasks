f5e577826d5ca7f51091eb75aa190a81cb64c419
Mironov <mironov5712s@gmail.com>
2020-11-11 14:35:24 +0200

    Create Druid.cs

---
+++ OOPPolymorphism09Nov2020Exercise/T03.Raiding/Druid.cs
@@ -0,0 +1,12 @@
+﻿namespace T03.Raiding
+{
+    public class Druid : BaseHero
+    {
+        private const int DRUID_POWER = 80;
+
+        public Druid(string name) : base(name, DRUID_POWER) { }
+
+        public override string CastAbility()
+            => $"{this.GetType().Name} - {this.Name} healed for {this.Power}";
+    }
+}
