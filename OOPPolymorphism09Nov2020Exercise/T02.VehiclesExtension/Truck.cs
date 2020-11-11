e93b3e25b34a6f94b04b683df0771333bda9294c
Mironov <mironov5712s@gmail.com>
2020-11-11 14:29:15 +0200

    Create Truck.cs

---
+++ OOPPolymorphism09Nov2020Exercise/T02.VehiclesExtension/Truck.cs
@@ -0,0 +1,14 @@
+﻿namespace T02.VehiclesExtension
+{
+    using System;
+
+    internal class Truck : Vehicle
+    {
+        private const double CONSUMPTION_MODIFIER = 1.6;
+
+        public Truck(double fuel, double consumption, double capacity)
+            : base(fuel, consumption + CONSUMPTION_MODIFIER, capacity)
+        {
+        }
+    }
+}
