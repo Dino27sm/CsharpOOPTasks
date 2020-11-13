40225b7aa4d3a25bd385b43f4f5e4c29373c9430
Mironov <mironov5712s@gmail.com>
2020-11-13 06:00:36 +0200

    Create Program.cs

---
+++ OOPPolymorphism09Nov2020Exercise/T03.Raiding/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T03.Raiding
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<BaseHero> raidMembers = new List<BaseHero>();
+            List<string> allLines = new List<string>();
+            int membersNum = int.Parse(Console.ReadLine());
+
+            string inpLine = "";
+            while (true)
+            {
+                inpLine = Console.ReadLine();
+                allLines.Add(inpLine);
+                if (char.IsDigit(inpLine[0]))
+                    break;
+            }
+
+            for (int i = 1; i < allLines.Count - 1; i += 2)
+            {
+                string heroName = allLines[i - 1];
+                string heroType = allLines[i];
+                if (raidMembers.Count < membersNum)
+                {
+                    try
+                    {
+                        BaseHero heroObject = new FactoryHeroes().CreateHero(heroName, heroType);
+                        raidMembers.Add(heroObject);
+                    }
+                    catch (ArgumentException ae)
+                    {
+                        Console.WriteLine(ae.Message);
+                    }
+                }
+                else break;
+            }
+
+            int bossPower = int.Parse(allLines.Last());
+            int sumPowerOfHeroes = 0;
+            foreach (BaseHero hero in raidMembers)
+            {
+                Console.WriteLine(hero.CastAbility());
+                sumPowerOfHeroes += hero.Power;
+            }
+            if (sumPowerOfHeroes >= bossPower)
+                Console.WriteLine("Victory!");
+            else
+                Console.WriteLine("Defeat...");
+        }
+    }
+}
