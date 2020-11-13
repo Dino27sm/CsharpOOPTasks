395fe4715c5067a67834638ea360b4af2b19932d
Mironov <mironov5712s@gmail.com>
2020-11-13 06:02:07 +0200

    Create Warrior.cs

---
+++ OOPPolymorphism09Nov2020Exercise/T03.Raiding/Warrior.cs
@@ -0,0 +1,12 @@
+﻿namespace T03.Raiding
+{
+    public class Warrior : BaseHero
+    {
+        private const int ROGUE_POWER = 100;
+
+        public Warrior(string name) : base(name, ROGUE_POWER) { }
+
+        public override string CastAbility()
+            => $"{this.GetType().Name} - {this.Name} hit for {this.Power} damage";
+    }
+}
