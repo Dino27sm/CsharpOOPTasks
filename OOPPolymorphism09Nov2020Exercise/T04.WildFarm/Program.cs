7e68ea17154ea76839509be1b93028efb3180eb9
Mironov <mironov5712s@gmail.com>
2020-11-13 06:07:44 +0200

    Create Program.cs

---
+++ OOPPolymorphism09Nov2020Exercise/T04.WildFarm/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using T04.WildFarm.Animals;
+using T04.WildFarm.Foods;
+using T04.WildFarm.Factories;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Collections.Generic;
+
+namespace T04.WildFarm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Animal> animalInfo = new HashSet<Animal>();
+            string firstLine = "";
+            while((firstLine = Console.ReadLine()) != "End")
+            {
+                Animal gotAnimal = new AnimalFactory().CreateAimal(firstLine);
+                Console.WriteLine(gotAnimal.AnimalSound());
+
+                string secondLine = Console.ReadLine();
+                Food gotFood = new FoodFactory().ProduceFood(secondLine);
+
+                if(!gotAnimal.IsMyFood(gotFood))
+                    Console.WriteLine($"{gotAnimal.GetType().Name} does not eat {gotFood.GetType().Name}!");
+
+                animalInfo.Add(gotAnimal);
+            }
+            foreach (Animal animal in animalInfo)
+            {
+                Console.WriteLine(animal);
+            }
+        }
+    }
+}
