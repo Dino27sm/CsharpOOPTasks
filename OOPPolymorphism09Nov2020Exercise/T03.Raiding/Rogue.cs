0fe579e847cf40fa46dc9219bf1009a9c9f046c2
Mironov <mironov5712s@gmail.com>
2020-11-13 06:01:15 +0200

    Create Rogue.cs

---
+++ OOPPolymorphism09Nov2020Exercise/T03.Raiding/Rogue.cs
@@ -0,0 +1,12 @@
+﻿namespace T03.Raiding
+{
+    public class Rogue : BaseHero
+    {
+        private const int ROGUE_POWER = 80;
+
+        public Rogue(string name) : base(name, ROGUE_POWER) { }
+
+        public override string CastAbility()
+            => $"{this.GetType().Name} - {this.Name} hit for {this.Power} damage";
+    }
+}
