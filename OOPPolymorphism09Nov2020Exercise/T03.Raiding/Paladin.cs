a4a0e9d460d57d368a1cc7114c9190bee312e3cd
Mironov <mironov5712s@gmail.com>
2020-11-13 05:59:48 +0200

    Create Paladin.cs

---
+++ OOPPolymorphism09Nov2020Exercise/T03.Raiding/Paladin.cs
@@ -0,0 +1,12 @@
+﻿namespace T03.Raiding
+{
+    public class Paladin : BaseHero
+    {
+        private const int PALADIN_POWER = 100;
+
+        public Paladin(string name) : base(name, PALADIN_POWER) { }
+
+        public override string CastAbility()
+            => $"{this.GetType().Name} - {this.Name} healed for {this.Power}";
+    }
+}
