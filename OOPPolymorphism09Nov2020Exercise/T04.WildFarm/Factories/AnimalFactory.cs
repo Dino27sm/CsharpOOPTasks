687d78f3b67993ebadcc4665dc4f47283c86a1a7
Mironov <mironov5712s@gmail.com>
2020-11-13 06:10:33 +0200

    Animal and Food factories

---
+++ OOPPolymorphism09Nov2020Exercise/T04.WildFarm/Factories/AnimalFactory.cs
@@ -0,0 +1,41 @@
+﻿namespace T04.WildFarm.Factories
+{
+    using Animals;
+    public class AnimalFactory
+    {
+        public Animal CreateAimal(string animalData)
+        {
+            string[] dataParts = animalData.Split(" ", System.StringSplitOptions.RemoveEmptyEntries);
+            string type = dataParts[0].ToLower();
+            string animalName = dataParts[1];
+            double animalWeight = double.Parse(dataParts[2]);
+            Animal animalObject = null;
+            if(type == "cat" || type == "tiger")
+            {
+                string livingRegion = dataParts[3];
+                string breed = dataParts[4];
+                if (type == "cat") 
+                    animalObject = new Cat(animalName, animalWeight, livingRegion, breed);
+                else if (type == "tiger")
+                    animalObject = new Tiger(animalName, animalWeight, livingRegion, breed);
+            }
+            else if (type == "dog" || type == "mouse")
+            {
+                string livingRegion = dataParts[3];
+                if (type == "dog")
+                    animalObject = new Dog(animalName, animalWeight, livingRegion);
+                else if (type == "mouse")
+                    animalObject = new Mouse(animalName, animalWeight, livingRegion);
+            }
+            else if (type == "hen" || type == "owl")
+            {
+                double wingSize = double.Parse(dataParts[3]);
+                if (type == "hen")
+                    animalObject = new Hen(animalName, animalWeight, wingSize);
+                else if (type == "owl")
+                    animalObject = new Owl(animalName, animalWeight, wingSize);
+            }
+            return animalObject;
+        } 
+    }
+}
---
+++ OOPPolymorphism09Nov2020Exercise/T04.WildFarm/Factories/FoodFactory.cs
@@ -0,0 +1,21 @@
+﻿namespace T04.WildFarm.Factories
+{
+    using Foods;
+
+    public class FoodFactory
+    {
+        public Food ProduceFood(string foodData)
+        {
+            string[] dataParts = foodData.Split(" ", System.StringSplitOptions.RemoveEmptyEntries);
+            string foodType = dataParts[0];
+            int foodAmount = int.Parse(dataParts[1]);
+            string type = foodType.ToLower();
+            Food foodObject = null;
+            if (type == "vegetable") foodObject = new Vegetable(foodAmount);
+            else if (type == "fruit") foodObject = new Fruit(foodAmount);
+            else if (type == "meat") foodObject = new Meat(foodAmount);
+            else if (type == "seeds") foodObject = new Seeds(foodAmount);
+            return foodObject;
+        }
+    }
+}
