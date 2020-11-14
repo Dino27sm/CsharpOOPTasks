1e8d60fd4258ad028661970a3a24ab2dc5cb9c9b
Mironov <mironov5712s@gmail.com>
2020-11-14 19:08:20 +0200

    Create Food.cs

---
+++ OOPPolymorphism09Nov2020Exercise/T04.WildFarm/Foods/Food.cs
@@ -0,0 +1,13 @@
+﻿namespace T04.WildFarm.Foods
+{
+    public abstract class Food
+    {
+        private int quantity;
+
+        public Food(int quantity)
+        {
+            this.Quantity = quantity;
+        }
+        public int Quantity { get; set; }
+    }
+}
