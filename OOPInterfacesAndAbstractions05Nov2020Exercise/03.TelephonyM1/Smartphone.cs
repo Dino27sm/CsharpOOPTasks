9ea3c8c6550c4c02f511b7f3f6deebd131ac5806
Mironov <mironov5712s@gmail.com>
2020-11-15 19:14:28 +0200

    Create Smartphone.cs

---
+++ OOPInterfacesAndAbstractions05Nov2020Exercise/03.TelephonyM1/Smartphone.cs
@@ -0,0 +1,13 @@
+﻿namespace _03.TelephonyM1
+{
+    using System.Linq;
+
+    public class Smartphone : IBrowsable
+    {
+        public bool BrowseSites(string webSites)
+            => (!webSites.ToCharArray().Any(x => char.IsDigit(x)));
+
+        public bool DialPhoneNumber(string phoneNumber)
+            => (phoneNumber.ToCharArray().All(x => char.IsDigit(x))) && phoneNumber.Length == 10;
+    }
+}
