cfeaf5f7c73e8feb8b7ebb871e734e01205d2221
Mironov <mironov5712s@gmail.com>
2020-11-15 19:20:42 +0200

    Create Citizen.cs

---
+++ OOPInterfacesAndAbstractions05Nov2020Exercise/04.BorderControl/Citizen.cs
@@ -0,0 +1,15 @@
+﻿namespace _04.BorderControl
+{
+    public class Citizen : ILiveable
+    {
+        public Citizen(string name, string age, string id)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Id = id;
+        }
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public string Id { get; set; }
+    }
+}
