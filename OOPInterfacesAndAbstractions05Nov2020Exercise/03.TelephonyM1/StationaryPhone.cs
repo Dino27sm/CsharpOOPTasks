47b6eff0479e7c0e8feebe3bf2f239f2e8e158ef
Mironov <mironov5712s@gmail.com>
2020-11-15 19:13:14 +0200

    Create StationaryPhone.cs

---
+++ OOPInterfacesAndAbstractions05Nov2020Exercise/03.TelephonyM1/StationaryPhone.cs
@@ -0,0 +1,10 @@
+﻿namespace _03.TelephonyM1
+{
+    using System.Linq;
+
+    public class StationaryPhone : IDialable
+    {
+        public bool DialPhoneNumber(string phoneNumber)
+            => (phoneNumber.ToCharArray().All(x => char.IsDigit(x))) && phoneNumber.Length == 7;
+    }
+}
