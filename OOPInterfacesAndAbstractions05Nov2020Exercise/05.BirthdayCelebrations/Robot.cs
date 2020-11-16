c96b5e9134d7ca141f8a0e895ff3ec010138679a
Mironov <mironov5712s@gmail.com>
2020-11-16 21:43:42 +0200

    Create Robot.cs

---
+++ OOPInterfacesAndAbstractions05Nov2020Exercise/05.BirthdayCelebrations/Robot.cs
@@ -0,0 +1,13 @@
+﻿namespace _05.BirthdayCelebrations
+{
+    public class Robot : IIdentifyable
+    {
+        public Robot(string model, string id)
+        {
+            this.Model = model;
+            this.Id = id;
+        }
+        public string Model { get; set; }
+        public string Id { get; set; }
+    }
+}
