a43921ae5fee1762446643537f17815a31d2dcf5
Mironov <mironov5712s@gmail.com>
2020-11-16 21:42:41 +0200

    Create Pet.cs

---
+++ OOPInterfacesAndAbstractions05Nov2020Exercise/05.BirthdayCelebrations/Pet.cs
@@ -0,0 +1,15 @@
+﻿namespace _05.BirthdayCelebrations
+{
+    public class Pet : ILiveable
+    {
+        public Pet(string name, string birthDate) 
+        {
+            this.Name = name;
+            this.Birthdate = birthDate;
+        }
+
+        public string Name { get; set; }
+        public string Birthdate { get; set; }
+        public string Id { get; set; }
+    }
+}
