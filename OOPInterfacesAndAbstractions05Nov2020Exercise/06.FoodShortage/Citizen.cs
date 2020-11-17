a47627b23d1a3ecacba1a15f124df180902218cc
Mironov <mironov5712s@gmail.com>
2020-11-17 22:11:52 +0200

    Create Citizen.cs

---
+++ OOPInterfacesAndAbstractions05Nov2020Exercise/06.FoodShortage/Citizen.cs
@@ -0,0 +1,20 @@
+﻿namespace _06.FoodShortage
+{
+    public class Citizen : ILiveable, IBuyer
+    {
+        public Citizen(string name, string age, string id, string birthdate)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Id = id;
+            this.Birthdate = birthdate;
+        }
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public string Id { get; set; }
+        public string Birthdate { get; set; }
+        public int Food { get; set; } = 0;
+
+        public virtual int BuyFood() => this.Food += 10;
+    }
+}
