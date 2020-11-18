76fd61cc7262db54f1109a731c62f96516c7fadf
Mironov <mironov5712s@gmail.com>
2020-11-18 22:34:34 +0200

    Create Rebel.cs

---
+++ OOPInterfacesAndAbstractions05Nov2020Exercise/06.FoodShortage/Rebel.cs
@@ -0,0 +1,14 @@
+﻿namespace _06.FoodShortage
+{
+    public class Rebel : Citizen
+    {
+        public Rebel(string name, string age, string group) 
+            : base(name, age, string.Empty, string.Empty)
+        {
+            this.Group = group;
+        }
+        public string Group { get; set; }
+
+        public override int BuyFood() => this.Food += 5;
+    }
+}
