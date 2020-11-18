2cc4bed6c763c50fe17e9bf761a91186413ed47c
Mironov <mironov5712s@gmail.com>
2020-11-18 22:32:54 +0200

    Create ILiveable.cs

---
+++ OOPInterfacesAndAbstractions05Nov2020Exercise/06.FoodShortage/ILiveable.cs
@@ -0,0 +1,8 @@
+﻿namespace _06.FoodShortage
+{
+    public interface ILiveable : IIdentifyable
+    {
+        public string Name { get; set; }
+        public string Birthdate { get; set; }
+    }
+}
