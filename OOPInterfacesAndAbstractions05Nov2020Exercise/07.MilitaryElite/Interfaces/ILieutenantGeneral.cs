ce651a84b94e36f01678490ca6d8d909d09e7d0a
Mironov <mironov5712s@gmail.com>
2020-11-19 20:17:32 +0200

    Create ILieutenantGeneral.cs

---
+++ OOPInterfacesAndAbstractions05Nov2020Exercise/07.MilitaryElite/Interfaces/ILieutenantGeneral.cs
@@ -0,0 +1,9 @@
+﻿namespace _07.MilitaryElite.Interfaces
+{
+    using System.Collections.Generic;
+
+    public interface ILieutenantGeneral
+    {
+        public ICollection<IPrivate> PrivatesList { get; set; }
+    }
+}
