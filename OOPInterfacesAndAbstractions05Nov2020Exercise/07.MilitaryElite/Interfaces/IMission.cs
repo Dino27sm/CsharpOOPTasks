53d3221cb37340cbac8b090bb7e4ed414c53b8f6
Mironov <mironov5712s@gmail.com>
2020-11-19 20:18:38 +0200

    Create IMission.cs

---
+++ OOPInterfacesAndAbstractions05Nov2020Exercise/07.MilitaryElite/Interfaces/IMission.cs
@@ -0,0 +1,12 @@
+﻿namespace _07.MilitaryElite.Interfaces
+{
+    using _07.MilitaryElite.Enumerators;
+
+    public interface IMission
+    {
+        public string CodeName { get; set; }
+        public MissionStateEnum MissionStateEnum { get; set; }
+
+        public void CompleteMission(string missionName);
+    }
+}
