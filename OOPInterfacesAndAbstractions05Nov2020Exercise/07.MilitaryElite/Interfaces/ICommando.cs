0944714fe984bcb3060b3af80cdbff667b380c15
Mironov <mironov5712s@gmail.com>
2020-11-19 20:12:38 +0200

    Create ICommando.cs

---
+++ OOPInterfacesAndAbstractions05Nov2020Exercise/07.MilitaryElite/Interfaces/ICommando.cs
@@ -0,0 +1,9 @@
+﻿namespace _07.MilitaryElite.Interfaces
+{
+    using System.Collections.Generic;
+
+    public interface ICommando
+    {
+        public ICollection<IMission> Missions { get; set; }
+    }
+}
