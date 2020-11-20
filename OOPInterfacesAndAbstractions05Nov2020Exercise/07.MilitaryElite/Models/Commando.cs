b2114fc43e626b35898893ff785d5d4092194b52
Mironov <mironov5712s@gmail.com>
2020-11-20 08:39:48 +0200

    Create Commando.cs

---
+++ OOPInterfacesAndAbstractions05Nov2020Exercise/07.MilitaryElite/Models/Commando.cs
@@ -0,0 +1,32 @@
+﻿namespace _07.MilitaryElite
+{
+    using _07.MilitaryElite.Enumerators;
+    using _07.MilitaryElite.Interfaces;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Commando : SpecialisedSoldier, ICommando
+    {
+        public Commando(int id, string firstName, string lastName, decimal salary,
+            CorpsEnumerator corpsEnum, ICollection<IMission> missions) 
+            : base(id, firstName, lastName, salary, corpsEnum)
+        {
+            this.Missions = missions;
+        }
+
+        public ICollection<IMission> Missions { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Name: {this.FirsName} {this.LastName} Id: {this.Id} Salary: {this.Salary:F2}");
+            sb.AppendLine($"Corps: {this.CorpsEnumerator}");
+            sb.AppendLine($"Missions:");
+            foreach (var mission in this.Missions)
+            {
+                sb.AppendLine($"  {mission.ToString()}");
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
