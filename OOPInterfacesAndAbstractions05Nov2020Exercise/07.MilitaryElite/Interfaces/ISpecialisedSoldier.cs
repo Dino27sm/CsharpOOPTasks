849c15b9a0df1af034318e27bc82474ad5652084
Mironov <mironov5712s@gmail.com>
2020-11-20 08:37:30 +0200

    Create ISpecialisedSoldier.cs

---
+++ OOPInterfacesAndAbstractions05Nov2020Exercise/07.MilitaryElite/Interfaces/ISpecialisedSoldier.cs
@@ -0,0 +1,9 @@
+﻿namespace _07.MilitaryElite.Interfaces
+{
+    using _07.MilitaryElite.Enumerators;
+
+    public interface ISpecialisedSoldier
+    {
+        CorpsEnumerator CorpsEnumerator { get; }
+    }
+}
