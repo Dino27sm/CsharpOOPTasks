c1cb122437b26d282149f91efb46fde7c0899c96
Mironov <mironov5712s@gmail.com>
2020-11-22 15:53:05 +0200

    Create Repair.cs

---
+++ OOPInterfacesAndAbstractions05Nov2020Exercise/07.MilitaryElite/Models/Repair.cs
@@ -0,0 +1,20 @@
+﻿namespace _07.MilitaryElite
+{
+    using _07.MilitaryElite.Interfaces;
+
+    public class Repair : IRepair
+    {
+        public Repair(string partName, int workedHours)
+        {
+            this.PartName = partName;
+            this.WorkedHours = workedHours;
+        }
+        public string PartName { get; set; }
+        public int WorkedHours { get; set; }
+
+        public override string ToString()
+        {
+            return $"Part Name: {this.PartName} Hours Worked: {this.WorkedHours}";
+        }
+    }
+}
