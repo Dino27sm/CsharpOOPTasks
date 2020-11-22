9dd91c45eacb4c2771d426d7e9bbdfe2afed41fc
Mironov <mironov5712s@gmail.com>
2020-11-22 15:51:58 +0200

    Create Mission.cs

---
+++ OOPInterfacesAndAbstractions05Nov2020Exercise/07.MilitaryElite/Models/Mission.cs
@@ -0,0 +1,26 @@
+﻿namespace _07.MilitaryElite
+{
+    using _07.MilitaryElite.Interfaces;
+    using _07.MilitaryElite.Enumerators;
+
+    public class Mission : IMission
+    {
+        public Mission(string codeName, MissionStateEnum missionStateEnum)
+        {
+            this.CodeName = codeName;
+            this.MissionStateEnum = missionStateEnum;
+        }
+        public string CodeName { get; set; }
+        public MissionStateEnum MissionStateEnum { get; set; }
+
+        public void CompleteMission(string missionName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"Code Name: {this.CodeName} State: {this.MissionStateEnum}";
+        }
+    }
+}
