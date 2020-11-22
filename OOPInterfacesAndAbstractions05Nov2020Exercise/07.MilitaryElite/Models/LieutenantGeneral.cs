a2828545e12a1b4e206357812b750bf329eeddb2
Mironov <mironov5712s@gmail.com>
2020-11-22 15:51:21 +0200

    Create LieutenantGeneral.cs

---
+++ OOPInterfacesAndAbstractions05Nov2020Exercise/07.MilitaryElite/Models/LieutenantGeneral.cs
@@ -0,0 +1,29 @@
+﻿namespace _07.MilitaryElite
+{
+    using _07.MilitaryElite.Interfaces;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class LieutenantGeneral : Private, ILieutenantGeneral
+    {
+        public LieutenantGeneral(int id, string firstName, string lastName, decimal salary, ICollection<IPrivate> privates) 
+            : base(id, firstName, lastName, salary)
+        {
+            this.PrivatesList = privates;
+        }
+
+        public ICollection<IPrivate> PrivatesList { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Name: {this.FirsName} {this.LastName} Id: {this.Id} Salary: {this.Salary:F2}");
+            sb.AppendLine($"Privates:");
+            foreach (var privateOne in this.PrivatesList)
+            {
+                sb.AppendLine($"  {privateOne.ToString()}");
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
