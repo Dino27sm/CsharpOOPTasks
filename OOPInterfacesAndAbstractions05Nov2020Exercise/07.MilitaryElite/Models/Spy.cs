fdcd8ebaba388b0bc8a33fb6bc1f7f510ba256d0
Mironov <mironov5712s@gmail.com>
2020-11-22 15:55:07 +0200

    Create Spy.cs

---
+++ OOPInterfacesAndAbstractions05Nov2020Exercise/07.MilitaryElite/Models/Spy.cs
@@ -0,0 +1,24 @@
+﻿namespace _07.MilitaryElite
+{
+    using System.Text;
+    using _07.MilitaryElite.Interfaces;
+
+    public class Spy : Soldier, ISpy
+    {
+        public Spy(int id, string firstName, string lastName, int codeNumber) 
+            : base(id, firstName, lastName)
+        {
+            this.CodeNumber = codeNumber;
+        }
+
+        public int CodeNumber { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Name: {this.FirsName} {this.LastName} Id: {this.Id}");
+            sb.AppendLine($"Code Number: {this.CodeNumber}");
+            return sb.ToString().Trim();
+        }
+    }
+}
