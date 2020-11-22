ce48c76362ea6bbe434edc654603df18cbf1d08b
Mironov <mironov5712s@gmail.com>
2020-11-22 15:53:34 +0200

    Create Soldier.cs

---
+++ OOPInterfacesAndAbstractions05Nov2020Exercise/07.MilitaryElite/Models/Soldier.cs
@@ -0,0 +1,17 @@
+﻿namespace _07.MilitaryElite
+{
+    using _07.MilitaryElite.Interfaces;
+
+    public abstract class Soldier : ISoldier
+    {
+        public Soldier(int id, string firstName, string lastName)
+        {
+            this.Id = id;
+            this.FirsName = firstName;
+            this.LastName = lastName;
+        }
+        public int Id { get; set; }
+        public string FirsName { get; set; }
+        public string LastName { get; set; }
+    }
+}
