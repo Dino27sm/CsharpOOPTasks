cc262bc7ae51a53cd993b0d15d6b04f4e4fb4d94
Mironov <mironov5712s@gmail.com>
2020-11-22 15:54:15 +0200

    Create SpecialisedSoldier.cs

---
+++ OOPInterfacesAndAbstractions05Nov2020Exercise/07.MilitaryElite/Models/SpecialisedSoldier.cs
@@ -0,0 +1,16 @@
+﻿namespace _07.MilitaryElite
+{
+    using _07.MilitaryElite.Enumerators;
+    using _07.MilitaryElite.Interfaces;
+
+    public abstract class SpecialisedSoldier : Private, ISpecialisedSoldier
+    {
+        public SpecialisedSoldier(int id, string firstName, string lastName, decimal salary, CorpsEnumerator corpsEnum) 
+            : base(id, firstName, lastName, salary)
+        {
+            this.CorpsEnumerator = corpsEnum;
+        }
+
+        public CorpsEnumerator CorpsEnumerator { get; }
+    }
+}
