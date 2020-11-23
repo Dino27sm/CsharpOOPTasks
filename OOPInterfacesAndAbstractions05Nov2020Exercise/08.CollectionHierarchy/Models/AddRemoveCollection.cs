08cdf1fc1a7cc43783de0d2daca42eebe85b539e
Mironov <mironov5712s@gmail.com>
2020-11-23 23:42:30 +0200

    Create AddRemoveCollection.cs

---
+++ OOPInterfacesAndAbstractions05Nov2020Exercise/08.CollectionHierarchy/Models/AddRemoveCollection.cs
@@ -0,0 +1,23 @@
+﻿namespace _08.CollectionHierarchy.Models
+{
+    using System;
+    using System.Linq;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class AddRemoveCollection<T> : AddCollection<T>
+    {
+        public override int Add(T element)
+        {
+            this.internalList.Insert(0, element);
+            return 0;
+        }
+
+        public virtual T Remove()
+        {
+            T removedElement = this.internalList.LastOrDefault();
+            this.internalList.RemoveAt(this.internalList.Count - 1);
+            return removedElement;
+        }
+    }
+}
