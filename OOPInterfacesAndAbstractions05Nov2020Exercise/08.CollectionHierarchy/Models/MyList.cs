72d162798c2a082feecdf7397b9084fecd0bf666
Mironov <mironov5712s@gmail.com>
2020-11-23 23:43:57 +0200

    Create MyList.cs

---
+++ OOPInterfacesAndAbstractions05Nov2020Exercise/08.CollectionHierarchy/Models/MyList.cs
@@ -0,0 +1,19 @@
+﻿namespace _08.CollectionHierarchy.Models
+{
+    using System;
+    using System.Linq;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class MyList<T> : AddRemoveCollection<T>
+    {
+        public int Used => this.internalList.Count;
+
+        public override T Remove()
+        {
+            T removedElement = this.internalList[0];
+            this.internalList.RemoveAt(0);
+            return removedElement;
+        }
+    }
+}
