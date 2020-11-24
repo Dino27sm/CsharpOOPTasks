5f499e7d4e89867873ff76d870c777d76b94a6e2
Mironov <mironov5712s@gmail.com>
2020-11-24 21:45:49 +0200

    Create StartUp.cs

---
+++ OOPInterfacesAndAbstractions05Nov2020Exercise/09.ExplicitInterfaces/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _09.ExplicitInterfaces
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                string getLine = Console.ReadLine();
+                if (getLine == "End" || string.IsNullOrWhiteSpace(getLine))
+                    break;
+                string[] inpParts = getLine.Split(" ");
+                string name = inpParts[0];
+                string country = inpParts[1];
+                int age = int.Parse(inpParts[2]);
+
+                Citizen citizen = new Citizen(name, country, age);
+                IPerson person = citizen;
+                Console.WriteLine(person.GetName());
+
+                IResident resident = citizen;
+                Console.WriteLine(resident.GetName());
+            }
+        }
+    }
+}
