1b1f47b86132be94e0e71fe64271dd4221a0d93b
Mironov <mironov5712s@gmail.com>
2020-11-24 21:44:50 +0200

    Create IPerson.cs

---
+++ OOPInterfacesAndAbstractions05Nov2020Exercise/09.ExplicitInterfaces/IPerson.cs
@@ -0,0 +1,13 @@
+﻿namespace _09.ExplicitInterfaces
+{
+    using System;
+    using System.Collections;
+
+    public interface IPerson
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public string GetName();
+    }
+}
