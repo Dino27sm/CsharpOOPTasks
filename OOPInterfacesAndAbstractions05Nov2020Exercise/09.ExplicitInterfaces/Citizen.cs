ed1bf55f33aa72432ed7f176b3a05bc507bdca43
Mironov <mironov5712s@gmail.com>
2020-11-24 21:44:14 +0200

    Create Citizen.cs

---
+++ OOPInterfacesAndAbstractions05Nov2020Exercise/09.ExplicitInterfaces/Citizen.cs
@@ -0,0 +1,21 @@
+﻿namespace _09.ExplicitInterfaces
+{
+    using System;
+    using System.Collections;
+
+    public class Citizen : IResident, IPerson
+    {
+        public Citizen(string name, string country, int age)
+        {
+            this.Name = name;
+            this.Country = country;
+            this.Age = age;
+        }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public int Age { get; set; }
+
+        string IResident.GetName() => $"Mr/Ms/Mrs {this.Name}";
+        string IPerson.GetName() => this.Name;
+    }
+}
