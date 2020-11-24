9cd7a6792eb5f01df9494c2275b85abda164487c
Mironov <mironov5712s@gmail.com>
2020-11-24 21:45:16 +0200

    Create IResident.cs

---
+++ OOPInterfacesAndAbstractions05Nov2020Exercise/09.ExplicitInterfaces/IResident.cs
@@ -0,0 +1,13 @@
+﻿namespace _09.ExplicitInterfaces
+{
+    using System;
+    using System.Collections;
+
+    public interface IResident
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+
+        public string GetName();
+    }
+}
