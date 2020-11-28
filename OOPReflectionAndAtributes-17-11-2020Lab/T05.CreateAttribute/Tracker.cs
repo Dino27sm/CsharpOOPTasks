8728d073cf1ab395dbfd2702a9786836bfe38d12
Mironov <mironov5712s@gmail.com>
2020-11-28 08:20:56 +0200

    Create Tracker.cs

---
+++ OOPReflectionAndAtributes-17-11-2020Lab/T05.CreateAttribute/Tracker.cs
@@ -0,0 +1,26 @@
+﻿namespace AuthorProblem
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            Type typeOfClass = typeof(StartUp);
+            MethodInfo[] methods = typeOfClass.GetMethods(BindingFlags.Public 
+                | BindingFlags.Instance | BindingFlags.Static);
+
+            foreach (MethodInfo method in methods)
+            {
+                if (method.CustomAttributes.Any(x => x.AttributeType == typeof(AuthorAttribute)))
+                {
+                    var attributes = method.GetCustomAttributes();
+                    foreach (AuthorAttribute attr in attributes)
+                        Console.WriteLine($"{method.Name} is written by {attr.Name}");
+                }
+            }
+        }
+    }
+}
