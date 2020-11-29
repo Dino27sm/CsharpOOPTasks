4d05458c777e5ea60189ef6ae8410c7fb93ed54b
Mironov <mironov5712s@gmail.com>
2020-11-29 12:15:26 +0200

    Create CommandInterpreter.cs

---
+++ OOPReflectionAndAtributes-19Nov2020Exercise/CommandPattern/Core/Models/CommandInterpreter.cs
@@ -0,0 +1,31 @@
+﻿using CommandPattern.Core.Contracts;
+using CommandPattern.Core.Commands;
+using System;
+using System.Reflection;
+using System.Linq;
+
+namespace CommandPattern.Core.Models
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] commandLine = args.Split(" ", System.StringSplitOptions.RemoveEmptyEntries);
+            string[] arguments = commandLine.Skip(1).ToArray();
+            string commanName = commandLine[0];
+
+            Type type = Assembly.GetCallingAssembly().GetTypes()
+                .FirstOrDefault(x => x.Name.StartsWith(commanName));
+
+            if (type == null)
+                throw new ArgumentException("Invalid command type!");
+
+            ICommand command = Activator.CreateInstance(type) as ICommand;
+
+            if (command == null)
+                throw new ArgumentException("Invalid command type!");
+
+            return command.Execute(arguments);
+        }
+    }
+}
