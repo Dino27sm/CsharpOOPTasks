7bf9f4a19149a21ec1c9ffdf5f25963ba7cb3b1d
Mironov <mironov5712s@gmail.com>
2020-11-30 21:31:47 +0200

    Create Person.cs

---
+++ OOPReflectionAndAtributes-19Nov2020Exercise/ValidationAttributes/Person.cs
@@ -0,0 +1,20 @@
+﻿namespace ValidationAttributes
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Person
+    {
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        [MyRequired]
+        public string Name { get; set; }
+
+        [MyRange(12, 90)]
+        public int Age { get; set; }
+    }
+}
