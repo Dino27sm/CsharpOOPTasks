9b750663bfa4f476218fcc396a5a3cc0e4666ec6
Mironov <mironov5712s@gmail.com>
2020-11-30 21:29:03 +0200

    Create MyRangeAttribute.cs

---
+++ OOPReflectionAndAtributes-19Nov2020Exercise/ValidationAttributes/MyRangeAttribute.cs
@@ -0,0 +1,30 @@
+﻿namespace ValidationAttributes
+{
+    using System;
+
+    public class MyRangeAttribute : MyValidationAttribute
+    {
+        private readonly int minValue;
+        private readonly int maxValue;
+
+        public MyRangeAttribute(int minValue, int maxValue)
+        {
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+        }
+
+        public override bool IsValid(object obj)
+        {
+            int parameterValue;
+            if (obj is int)
+                parameterValue = (int)obj;
+            else
+                throw new ArgumentException("Invalid argument type!");
+
+            if (parameterValue >= this.minValue && parameterValue <= maxValue)
+                return true;
+            else
+                return false;
+        }
+    }
+}
