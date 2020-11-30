7defd0602b7034719cbab99a2f32dd05f66effbb
Mironov <mironov5712s@gmail.com>
2020-11-30 21:27:29 +0200

    Create Validator.cs

---
+++ OOPReflectionAndAtributes-19Nov2020Exercise/ValidationAttributes/Validator.cs
@@ -0,0 +1,29 @@
+﻿namespace ValidationAttributes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
+    public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            PropertyInfo[] objProperties = obj.GetType().GetProperties();
+            foreach (PropertyInfo propertyInfo in objProperties)
+            {
+                IEnumerable<MyValidationAttribute> propertyAttr = propertyInfo.GetCustomAttributes()
+                    .Where(x => x is MyValidationAttribute)
+                    .Cast<MyValidationAttribute>();
+
+                foreach (MyValidationAttribute attr in propertyAttr)
+                {
+                    bool isValid = attr.IsValid(propertyInfo.GetValue(obj));
+                    if (!isValid)
+                        return false;
+                }
+            }
+            return true;
+        }
+    }
+}
