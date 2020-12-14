6ed258712c5bae537141cd97eaa35231d4a7ca56
Mironov <70315066+Dino27sm@users.noreply.github.com>
2020-12-14 23:39:32 +0200

    Update RobotsTests.cs

--- OldExamOOP12April2020/UnitTests/Robots.Tests/RobotsTests.cs
+++ OldExamOOP12April2020/UnitTests/Robots.Tests/RobotsTests.cs
@@ -33,7 +33,7 @@
         }
 
         [Test]
-        public void RorotMng_Constructor_Initial()
+        public void RobotMng_Constructor_Initial()
         {
             RobotManager rm1 = new RobotManager(70);
 
@@ -42,7 +42,7 @@
         }
 
         [Test]
-        public void RorotMng_Constructor_Initial_Zero_Allowed()
+        public void RobotMng_Constructor_Initial_Zero_Allowed()
         {
             RobotManager rm1 = new RobotManager(0);
 
@@ -51,7 +51,7 @@
         }
 
         [Test]
-        public void RorotMng_Constructor_Negative_Capacity_Throw_Excep()
+        public void RobotMng_Constructor_Negative_Capacity_Throw_Excep()
         {
             Assert.Throws<ArgumentException>(() => new RobotManager(-8));
         }
