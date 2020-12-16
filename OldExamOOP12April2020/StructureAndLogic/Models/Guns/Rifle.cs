ecfa567aa2724ff0197c0e3752313fc2d17b2f8e
Mironov <mironov5712s@gmail.com>
2020-12-16 13:00:25 +0200

    Create Rifle.cs

---
+++ OldExamOOP12April2020/StructureAndLogic/Models/Guns/Rifle.cs
@@ -0,0 +1,27 @@
+﻿namespace CounterStrike.Models.Guns
+{
+    public class Rifle : Gun
+    {
+        private int bulletRate = 10;
+
+        public Rifle(string name, int bulletsCount)
+            : base(name, bulletsCount)
+        {
+        }
+
+        public override int Fire()  //Check for available bullets - if there are any -> shoots with given rate !!!
+        {
+            if (this.BulletsCount - bulletRate >= 0)
+            {
+                this.BulletsCount -= bulletRate;
+                return bulletRate;
+            }
+            else
+            {
+                int remainBullets = this.BulletsCount;
+                this.BulletsCount = 0;
+                return remainBullets;
+            }
+        }
+    }
+}
