e78c7e75eec98f562731bc7a0b217ecc6616dc7d
Mironov <mironov5712s@gmail.com>
2020-12-16 12:59:22 +0200

    Create Gun.cs

---
+++ OldExamOOP12April2020/StructureAndLogic/Models/Guns/Gun.cs
@@ -0,0 +1,33 @@
+﻿using CounterStrike.Models.Guns.Contracts;
+using CounterStrike.Utilities.Messages;
+using System;
+
+namespace CounterStrike.Models.Guns
+{
+    public abstract class Gun : IGun
+    {
+        private string name;
+        private int bulletsCount;
+
+        public Gun(string name, int bulletsCount)
+        {
+            this.Name = name;
+            this.BulletsCount = bulletsCount;
+        }
+
+        public string Name 
+        { 
+            get => this.name;
+            private set => this.name = !string.IsNullOrWhiteSpace(value) ? value
+                : throw new ArgumentException(ExceptionMessages.InvalidGunName);
+        }
+        public int BulletsCount 
+        { 
+            get => this.bulletsCount;
+            protected set => this.bulletsCount = value >= 0 ? value 
+                : throw new ArgumentException(ExceptionMessages.InvalidGunBulletsCount);
+        }
+
+        public abstract int Fire();
+    }
+}
