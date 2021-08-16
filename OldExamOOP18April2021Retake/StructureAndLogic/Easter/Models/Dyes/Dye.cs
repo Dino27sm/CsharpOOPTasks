907caff2805e619d658f2a34e464e43745231d60
Mironov <mironov5712s@gmail.com>
2021-08-16 08:55:05 +0300

    Create Dye.cs

---
+++ OldExamOOP18April2021Retake/StructureAndLogic/Easter/Models/Dyes/Dye.cs
@@ -0,0 +1,23 @@
+﻿using Easter.Models.Dyes.Contracts;
+
+namespace Easter.Models.Dyes
+{
+    public class Dye : IDye
+    {
+        private int power;
+
+        public Dye(int power)
+        {
+            this.Power = power;
+        }
+
+        public int Power 
+        { 
+            get => this.power; 
+            private set => this.power = value < 0 ? 0 : value; 
+        }
+
+        public bool IsFinished() => this.power == 0;
+        public void Use() => this.Power -= 10;
+    }
+}
