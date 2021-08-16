84bd32f623830393f8b66a521120d7920ea55e10
Mironov <mironov5712s@gmail.com>
2021-08-16 08:58:04 +0300

    Create Workshop.cs

---
+++ OldExamOOP18April2021Retake/StructureAndLogic/Easter/Models/Workshops/Workshop.cs
@@ -0,0 +1,30 @@
+﻿using Easter.Models.Bunnies.Contracts;
+using Easter.Models.Dyes;
+using Easter.Models.Eggs.Contracts;
+using Easter.Models.Workshops.Contracts;
+using System.Linq;
+
+namespace Easter.Models.Workshops
+{
+    public class Workshop : IWorkshop
+    {
+        public Workshop()
+        {
+
+        }
+        public void Color(IEgg egg, IBunny bunny)
+        {
+            Dye gotDye = (Dye)bunny.Dyes.FirstOrDefault(x => !x.IsFinished());
+            while (bunny.Energy > 0 && !egg.IsDone() && gotDye != null)
+            {
+                bunny.Work();
+                egg.GetColored();
+                gotDye.Use();
+                if (gotDye.IsFinished())
+                {
+                    gotDye = (Dye)bunny.Dyes.FirstOrDefault(x => !x.IsFinished());
+                }
+            }
+        }
+    }
+}
