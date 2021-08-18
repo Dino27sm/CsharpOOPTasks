e4cba83b9c300d6f81ee664026f61cdddc0e7faf
Mironov <mironov5712s@gmail.com>
2021-08-18 10:38:38 +0300

    Create EggRepository.cs

---
+++ OldExamOOP18April2021Retake/StructureAndLogic/Easter/Repositories/EggRepository.cs
@@ -0,0 +1,31 @@
+﻿using Easter.Models.Eggs.Contracts;
+using Easter.Repositories.Contracts;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Easter.Repositories
+{
+    public class EggRepository : IRepository<IEgg>
+    {
+        private readonly List<IEgg> models;
+
+        public EggRepository()
+        {
+            this.models = new List<IEgg>();
+        }
+
+        public IReadOnlyCollection<IEgg> Models => this.models.AsReadOnly();
+
+        public void Add(IEgg model)
+        {
+            if (!(model == null || this.models.Any(x => x.Name == model.Name)))
+            {
+                this.models.Add(model);
+            }
+        }
+
+        public IEgg FindByName(string name) => this.models.FirstOrDefault(x => x.Name == name);
+
+        public bool Remove(IEgg model) => this.models.Remove(model);
+    }
+}
