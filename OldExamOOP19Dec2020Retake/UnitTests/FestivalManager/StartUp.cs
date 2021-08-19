8254f74388625dd829df9f387a13b017f9fe99f1
Mironov <mironov5712s@gmail.com>
2021-08-19 07:09:28 +0300

    Create StartUp.cs

---
+++ OldExamOOP19Dec2020Retake/UnitTests/FestivalManager/StartUp.cs
@@ -0,0 +1,26 @@
+﻿namespace FestivalManager
+{	
+	using Entities;
+	using System;
+
+	public static class StartUp
+	{
+		public static void Main(string[] args)
+		{
+			//example usage of the Stage class
+			var song1 = new Song("Winds", new TimeSpan(0,3,30));
+			var song2 = new Song("Nights", new TimeSpan(0, 2, 30));
+
+			var performer = new Performer("Ivan", "Ivanov", 19);
+			Stage stage = new Stage();
+
+			stage.AddSong(song1);
+			stage.AddSong(song2);
+			stage.AddPerformer(performer);
+
+            Console.WriteLine(stage.AddSongToPerformer("Winds", "Ivan Ivanov"));
+
+            Console.WriteLine(stage.Play());
+		}
+	}
+}
