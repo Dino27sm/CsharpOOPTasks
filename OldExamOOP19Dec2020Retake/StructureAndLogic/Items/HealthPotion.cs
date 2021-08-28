fe39184e22d166b4c8fd8b9520959d0d9856cfaf
Mironov <mironov5712s@gmail.com>
2021-08-28 11:25:24 +0300

    Create HealthPotion.cs

---
+++ OldExamOOP19Dec2020Retake/StructureAndLogic/Items/HealthPotion.cs
@@ -0,0 +1,22 @@
+﻿
+using WarCroft.Entities.Characters.Contracts;
+
+namespace WarCroft.Entities.Items
+{
+    public class HealthPotion : Item
+    {
+        public HealthPotion() 
+            : base(5)
+        {
+        }
+
+        public override void AffectCharacter(Character character)
+        {
+            base.AffectCharacter(character);
+            if (character.Health + 20 > character.BaseHealth)
+                character.Health = character.BaseHealth;
+            else
+                character.Health += 20;
+        }
+    }
+}
